ef4d9a054126f95157f480fa1beb51ec4f3f2886
Harvey Ezihe <don4dolex@gmail.com>
2024-10-26 05:05:26 +0100

    Add custom layout for root pages and add navbar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,11 @@
+import NavBar from "../components/NavBar";
+
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-	return <main className="font-work-sans">{children}</main>;
+	return (
+		<main className="font-work-sans">
+			<NavBar />
+			{children}
+		</main>
+	);
 };
 export default Layout;
