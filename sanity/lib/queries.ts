4e94a4db3677830f4fd23d1cc77440f74bbfeb55
Harvey Ezihe <don4dolex@gmail.com>
2024-11-04 06:45:44 +0000

    Add new search query and generate types for search query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -9,8 +9,24 @@ export const STARTUPS_QUERY =
   author -> {
     _id, name, image, bio
   },
-  views, description, category, image
+  views, 
+  description, 
+  category, 
+  image
 }`);
 
-
-//  && !defined($search) || title match $search || category match $search || author->name match $search
+export const STARTUP_BY_ID_QUERY =
+	defineQuery(`*[_type == "startup" && _id == $id] [0]{
+  _id, 
+  title, 
+  slug, 
+  _createdAt, 
+  author -> {
+    _id, name, username, image, bio
+  },
+  views, 
+  description, 
+  category, 
+  image, 
+  pitch
+}`);
