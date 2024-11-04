c47a916d6a5268ebeb4dd45e4dce522cfed71644
Harvey Ezihe <don4dolex@gmail.com>
2024-11-04 06:08:22 +0000

    Refactor code to filter fetched data with searchParams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,27 +13,16 @@ const Home = async ({
 	// Get the search query from the URL
 	const { query } = await searchParams;
 
+	// Define the search params
+	const params = { search: query || null };
+
 	// Get data from sanity
 
-	// THIS FETCH IS THE DEFAULT WAY OF FETCHING DATA FROM SANITY. 
+	// THIS FETCH IS THE DEFAULT WAY OF FETCHING DATA FROM SANITY.
 	// const posts = await client.fetch<StartupCardType[]>(STARTUPS_QUERY)
 
 	// THIS FETCH IS THE LIVE WAY OF FETCHING DATA FROM SANITY. NEW DATA WILL BE FETCHED IN REAL TIME
-	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
-
-	// const posts = [
-	// 	{
-	// 		_createdAt: new Date(),
-	// 		views: 55,
-	// 		author: { _id: 1, name: "John Doe" },
-	// 		_id: 1,
-	// 		description: "A platform for connecting entrepreneurs with investors",
-	// 		image:
-	// 			"https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	// 		category: "Tech",
-	// 		title: "Startup Connect",
-	// 	},
-	// ];
+	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
 	return (
 		<>
@@ -53,7 +42,6 @@ const Home = async ({
 
 				<ul className="mt-7 card_grid">
 					{posts?.length > 0 ? (
-						// @ts-expect-error-Type 'StartupCardType' is not assignable to type 'string'
 						posts.map((post: StartupCardType) => (
 							<StartupCard key={post?._id} post={post} />
 						))
